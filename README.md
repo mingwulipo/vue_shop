b595534e7991461f3b9df1a7f2d16b2f20a1130b
lipo <you@example.com>
2020-02-02 16:02:16 +0800

    login

--- README.md
+++ README.md
@@ -22,3 +22,10 @@ npm run lint
 
 ### Customize configuration
 See [Configuration Reference](https://cli.vuejs.org/config/).
+
+bilibili的vue项目视频，黑马的
+https://www.bilibili.com/video/av74592164?p=22
+
+前端项目地址：http://shop.liulongbin.top/
+后端接口地址：https://www.liulongbin.top:8888/api/private/v1/
+
---
+++ src/components/Login.vue
@@ -0,0 +1,78 @@
+<template>
+  <div class="login_container">
+    <div class="login_box">
+      <el-form ref="loginFormRef" :rules="loginFormRules" label-width="80px" :model="loginForm">
+        <el-form-item prop="username">
+          <el-input v-model="loginForm.username" placeholder="输入用户名" prefix-icon="el-icon-user"></el-input>
+        </el-form-item>
+        <el-form-item prop="password">
+          <el-input
+            v-model="loginForm.password"
+            placeholder="输入密码"
+            prefix-icon="el-icon-lock"
+            show-password
+          ></el-input>
+        </el-form-item>
+        <el-form-item>
+          <el-button type="primary" @click="login">登录</el-button>
+          <el-button type="info" @click="resetLoginForm">重置</el-button>
+        </el-form-item>
+      </el-form>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      loginForm: {
+        username: 'admin',
+        password: '123456'
+      },
+      loginFormRules: {
+        username: [
+          { required: true, message: '请输入用户名', trigger: 'blur' },
+          { min: 3, max: 5, message: '长度在 3 到 5 个字符', trigger: 'blur' }
+        ],
+        password: [
+          { required: true, message: '请输入密码', trigger: 'blur' },
+          { min: 6, max: 15, message: '长度在 6 到 15 个字符', trigger: 'blur' }
+        ]
+      }
+    }
+  },
+  methods: {
+    resetLoginForm () {
+      this.$refs.loginFormRef.resetFields()
+    },
+    login () {
+      this.$refs.loginFormRef.validate(async valid => {
+        if (!valid) return
+        const { data: res } = await this.$http.post('login', this.loginForm)
+        if (res.meta.status !== 200) return this.$message.error('登录失败')
+        this.$message.success('登录成功')
+        window.sessionStorage.setItem('token', res.data.token)
+        this.$router.push('/home')
+      })
+    }
+  }
+}
+</script>
+
+<style lang="less" scoped>
+.login_container {
+  background-color: #2b4b2b;
+  height: 100%;
+}
+.login_box {
+  width: 450px;
+  height: 450px;
+  background-color: #fff;
+  border-radius: 3px;
+  position: absolute;
+  left: 50%;
+  top: 50%;
+  transform: translate(-50%, -50%);
+}
+</style>
